c719c6fd413386d966a336ae3805626ed2fba44f
Rodrigo Lins <rlinsdev@gmail.com>
2022-07-31 11:30:17 -0300

    Verbose security

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 17:50:23 by rlins             #+#    #+#             */
-/*   Updated: 2022/07/31 11:22:02 by rlins            ###   ########.fr       */
+/*   Updated: 2022/07/31 11:26:26 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 #include <mlx.h>
 # define WINDOW_WIDTH 600
 # define WINDOW_HEIGHT 300
+#define MLX_ERROR 1
 
 // Debug: clang main.c -lX11 -lXext -lmlx
 // OK  - valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./a.out 
+
 
 int main(void)
 {
@@ -24,11 +26,16 @@ int main(void)
 	void	*win_ptr;
 
 	mlx_ptr = mlx_init();
+	if (mlx_ptr == NULL)
+		return (MLX_ERROR);
 	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
-	while(1)
+	if (win_ptr == NULL)
 	{
-
+		free(win_ptr);
+		return (MLX_ERROR);
 	}
+	while (1)
+		;
 	mlx_destroy_window(mlx_ptr, win_ptr);
 	mlx_destroy_display(mlx_ptr);
 	free(mlx_ptr);
