5b5de67e2c8c8ad1ad9229ac982a848a7acf5ebb
Rodrigo Lins <rlinsdev@gmail.com>
2022-07-31 11:23:07 -0300

    My first windows

--- main.c
+++ main.c
@@ -6,21 +6,30 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 17:50:23 by rlins             #+#    #+#             */
-/*   Updated: 2022/07/31 10:22:39 by rlins            ###   ########.fr       */
+/*   Updated: 2022/07/31 11:22:02 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <mlx.h>
+# define WINDOW_WIDTH 600
+# define WINDOW_HEIGHT 300
 
 // Debug: clang main.c -lX11 -lXext -lmlx
 // OK  - valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./a.out 
 
-int	main(void)
+int main(void)
 {
 	void	*mlx_ptr;
+	void	*win_ptr;
 
 	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+	while(1)
+	{
+
+	}
+	mlx_destroy_window(mlx_ptr, win_ptr);
 	mlx_destroy_display(mlx_ptr);
 	free(mlx_ptr);
 }
