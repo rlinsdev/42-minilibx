c0eb60822160b7873b9326ca2ce2fdf205f2d90f
Rodrigo Lins <rlinsdev@gmail.com>
2022-07-31 12:13:57 -0300

    Events

--- main.c
+++ main.c
@@ -6,19 +6,30 @@
 /*   By: rlins <rlins@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/30 17:50:23 by rlins             #+#    #+#             */
-/*   Updated: 2022/07/31 11:26:26 by rlins            ###   ########.fr       */
+/*   Updated: 2022/07/31 12:10:23 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <mlx.h>
+#include <unistd.h>
 # define WINDOW_WIDTH 600
 # define WINDOW_HEIGHT 300
 #define MLX_ERROR 1
 
-// Debug: clang main.c -lX11 -lXext -lmlx
+// compile: clang main.c -lX11 -lXext -lmlx
 // OK  - valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./a.out 
 
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	deal_key(int key, void *param)
+{
+	ft_putchar('X');
+	return(0);
+}
 
 int main(void)
 {
@@ -34,6 +45,10 @@ int main(void)
 		free(win_ptr);
 		return (MLX_ERROR);
 	}
+
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
+	mlx_key_hook(win_ptr, deal_key, (void *)0);
+
 	while (1)
 		;
 	mlx_destroy_window(mlx_ptr, win_ptr);
